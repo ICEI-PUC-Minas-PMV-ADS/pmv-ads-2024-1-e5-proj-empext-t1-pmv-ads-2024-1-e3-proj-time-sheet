a342277b213377cc5f51cbab94601f3dc2510723
RaulShinaede <rshinaede@outlook.com>
2024-04-05 11:37:41 -0300

    Implementando comando de atualização do usuário

---
+++ codigo-fonte/TimeSheet.UnitTests/Commands/UpdateUserCommandHandlerTests.cs
@@ -0,0 +1,74 @@
+﻿using TimeSheet.Builders;
+using TimeSheet.Commands;
+using TimeSheet.Repositories;
+
+namespace TimeSheet.UnitTests.Commands {
+
+    [TestClass]
+    public class UpdateUserCommandHandlerTests {
+
+        private readonly UpdateUserCommandHandler _handler;
+        public UpdateUserCommandHandlerTests() {
+
+            var repository = new UserRepository(DevSettings.GetInMemoryContext());
+            var builder = new UserBuilder(new Services.PasswordService());
+
+            _handler = new UpdateUserCommandHandler(repository, builder);
+        }
+
+
+        [TestMethod]
+        public void Handle_UserThatNotExists_ReturnsFailureWithUserNotFoundStatus() {
+
+            var command = new UpdateUserCommand {
+                UserId = Guid.NewGuid(),
+                CPF = "61154646092",
+                Name = "Bruce Wayne",
+                TotalTime = 8,
+                LunchTime = 2,
+                Role = Models.UserRole.Administrator
+            };
+            var commandResult = _handler.Handle(command).RunSync();
+
+
+            Assert.IsNotNull(commandResult);
+            Assert.AreEqual(commandResult.Status, UpdateUserCommandResultStatus.UserNotFound);
+        }
+
+        [TestMethod]
+        public void Handle_InvalidUserData_ReturnsFailureWithInvalidUserDataStatus() {
+
+            var command = new UpdateUserCommand {
+                UserId = Guid.Parse("ba56273d-0c8b-4ea6-90ac-691494d1f402"),
+                CPF = "11111111111",
+                Name = "B",
+                TotalTime = 25,
+                LunchTime = 27,
+                Role = Models.UserRole.Administrator
+            };
+            var commandResult = _handler.Handle(command).RunSync();
+
+
+            Assert.IsNotNull(commandResult);
+            Assert.AreEqual(commandResult.Status, UpdateUserCommandResultStatus.InvalidUserData);
+        }
+
+        [TestMethod]
+        public void Handle_ValidUserData_ReturnsSuccess() {
+
+            var command = new UpdateUserCommand {
+                UserId = Guid.Parse("ba56273d-0c8b-4ea6-90ac-691494d1f402"),
+                CPF = "44309223060",
+                Name = "Bruce Wayne",
+                TotalTime = 10,
+                LunchTime = 3,
+                Role = Models.UserRole.Employee
+            };
+            var commandResult = _handler.Handle(command).RunSync();
+
+
+            Assert.IsNotNull(commandResult);
+            Assert.AreEqual(commandResult.Status, UpdateUserCommandResultStatus.UserUpdated);
+        }
+    }
+}
---
+++ codigo-fonte/TimeSheet/Commands/UpdateUserCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using TimeSheet.Builders;
+using TimeSheet.Repositories;
+
+namespace TimeSheet.Commands {
+    public class UpdateUserCommandHandler
+        : ICommandHandler<UpdateUserCommand, UpdateUserCommandResult> {
+
+        private readonly UserRepository _repository;
+        private readonly UserBuilder _builder;
+        public UpdateUserCommandHandler(UserRepository repository, UserBuilder builder) {
+            _repository = repository;
+            _builder = builder;
+        }
+
+        public async Task<UpdateUserCommandResult> Handle(UpdateUserCommand command) {
+
+            var user = await _repository
+                .FindUser(command.UserId);
+
+            if (user is null) {
+                return new UpdateUserCommandResult {
+                    Message = "Usuário não encontrado",
+                    Status = UpdateUserCommandResultStatus.UserNotFound
+                };
+            }
+
+            var result = _builder
+                .CreateNew()
+                .WithName(command.Name)
+                .WithCPF(command.CPF)
+                .WithWorkJourney(command.TotalTime, command.LunchTime)
+                .WithRole(command.Role)
+                .Build();
+
+            if (result.IsFailed) {
+                return new UpdateUserCommandResult {
+                    Message = "Valores inválidos.",
+                    Errors = result.Errors,
+                    Status = UpdateUserCommandResultStatus.InvalidUserData
+                };
+            }
+
+            user.CPF = result.Value.CPF;
+            user.Name = result.Value.Name;
+            user.Role = result.Value.Role;
+            user.LunchTime = result.Value.LunchTime;
+            user.TotalTime = result.Value.TotalTime;
+
+            await _repository.SaveChanges();
+
+            return new UpdateUserCommandResult {
+                Message = "Usuário atualizado.",
+                Status = UpdateUserCommandResultStatus.UserUpdated
+            };
+        }
+    }
+}
