95f9fc0bc3b22e6630071318c44a4b7c66e7d8fe
RaulShinaede <rshinaede@outlook.com>
2024-05-08 22:19:04 -0300

    Adicionando projeto de testes

---
+++ codigo-fonte/time-sheet-api/TimeSheet.UnitTests/Commands/CreateUserCommandHandlerTests.cs
@@ -0,0 +1,71 @@
+﻿using TimeSheet.Builders;
+using TimeSheet.Commands;
+using TimeSheet.Repositories;
+using TimeSheet.Services;
+
+namespace TimeSheet.UnitTests.Commands {
+    [TestClass]
+    public class CreateUserCommandHandlerTests {
+
+        private readonly CreateUserCommandHandler _handler;
+        public CreateUserCommandHandlerTests() {
+
+            var builder = new UserBuilder(new PasswordService());
+            var repository = new UserRepository(DevSettings.GetInMemoryContext());
+
+            _handler = new CreateUserCommandHandler(builder, repository);
+        }
+
+        [TestMethod]
+        public void Handle_InvalidCommandData_ReturnsFailureWithInvalidUserDataStatus() {
+
+            var command = new CreateUserCommand {
+                Name = "a",
+                CPF = "11111111111",
+                Password = "c",
+                LunchTime = 5,
+                TotalTime = .30,
+                Role = Models.UserRole.Employee
+            };
+
+            var commandResult = _handler.Handle(command).RunSync();
+
+            Assert.IsNotNull(commandResult);
+            Assert.AreEqual(commandResult.Status, CommandResultStatus.InvalidUserData);
+        }
+
+        [TestMethod]
+        public void Handle_UserThatAlreadyExists_ReturnsFailureWithUserAlreadyExistsStatus() {
+            var command = new CreateUserCommand {
+                Name = "Bruce Wayne",
+                CPF = "04037535033",
+                Password = "Teste@123",
+                TotalTime = 9,
+                LunchTime = 2,
+                Role = Models.UserRole.Administrator
+            };
+
+            var commandResult = _handler.Handle(command).RunSync();
+
+            Assert.IsNotNull(commandResult);
+            Assert.AreEqual(commandResult.Status, CommandResultStatus.UserAlreadyExists);
+        }
+
+        [TestMethod]
+        public void Handle_NewUser_ReturnsSuccessWithUserCreatedStatus() {
+            var command = new CreateUserCommand {
+                Name = "Raul Oliveira",
+                CPF = "36133786043",
+                Password = "Teste@123",
+                TotalTime = 8,
+                LunchTime = 1.30,
+                Role = Models.UserRole.Employee
+            };
+
+            var commandResult = _handler.Handle(command).RunSync();
+
+            Assert.IsNotNull(commandResult);
+            Assert.AreEqual(commandResult.Status, CommandResultStatus.UserCreated);
+        }
+    }
+}
---
+++ codigo-fonte/time-sheet-api/TimeSheet.UnitTests/DevSettings.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using TimeSheet.Configuration;
+using TimeSheet.Infrastructure;
+using TimeSheet.Models;
+using TimeSheet.Services;
+
+namespace TimeSheet.UnitTests {
+    public static class DevSettings {
+        public static AppOptions GetAppOptions() {
+            return new AppOptions {
+                ConnectionString = string.Empty,
+                JwtBearer = new JwtBearerOptions {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    SecretKey = "115d6db863da2f536f8534e8c7f84ee0f5925a38e8c029a0140d6cfa45e6713c",
+                    Expires = 8
+                }
+            };
+        }
+        private static TimeSheetContext InMemoryContextInstance;
+        public static TimeSheetContext GetInMemoryContext() {
+
+            if (InMemoryContextInstance is not null) {
+                return InMemoryContextInstance;
+            }
+
+            var options = new DbContextOptionsBuilder<TimeSheetContext>().
+                UseInMemoryDatabase("time-sheet").
+                UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking).
+                Options;
+
+            var context = new TimeSheetContext(options);
+
+            SeedDatabase(context);
+
+            InMemoryContextInstance = context;
+
+            return context;
+        }
+        private static void SeedDatabase(TimeSheetContext context) {
+            context.Users.AddRange(DefaultUsers());
+            context.SaveChanges();
+
+            context.ChangeTracker.Clear();
+        }
+        private static List<User> DefaultUsers() {
+
+            var passwordService = new PasswordService();
+
+            return new List<User> {
+                new User() {
+                    Id = Guid.Parse("ba56273d-0c8b-4ea6-90ac-691494d1f402"),
+                    Name = "Bruce Wayne",
+                    CPF = "04037535033",
+                    TotalTime = 9,
+                    LunchTime = 2,
+                    Password = passwordService.EncryptPassword("Teste@123"),
+                    Role = UserRole.Administrator,
+                    Status = UserStatus.Active
+                },
+                new User() {
+                    Id = Guid.Parse("b6a5e02a-40cd-4a47-960c-1a189ecd821a"),
+                    Name = "Robin Francis",
+                    CPF = "12995490041",
+                    TotalTime = 8,
+                    LunchTime = 1.30,
+                    Password = passwordService.EncryptPassword("Teste@123"),
+                    Role  = UserRole.Employee,
+                    Status = UserStatus.Inactive
+                },
+                new User() {
+                    Id = Guid.Parse("b6a5e04a-60cd-4a47-960c-1a189ecd221a"),
+                    Name = "Maria Julieta",
+                    CPF = "31207413020",
+                    TotalTime = 8,
+                    LunchTime = 1.0,
+                    Password = passwordService.EncryptPassword("Senha@123"),
+                    Role  = UserRole.Employee,
+                    Status = UserStatus.Active
+                },
+            };
+        }
+    }
+
+    public static class AsyncHelper {
+        public static TResult RunSync<TResult>(this Task<TResult> task) {
+            return task.GetAwaiter().GetResult();
+        }
+    }
+    public static class StringUtils {
+        public static string CreateLongString(string pattern, int times) {
+
+            var stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < times; i++) {
+                stringBuilder.Append(pattern);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
