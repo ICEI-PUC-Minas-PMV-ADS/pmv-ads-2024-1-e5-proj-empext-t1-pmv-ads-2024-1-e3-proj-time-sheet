384a7da820e7d7509e177d1ca30419871b85762e
RaulShinaede <rshinaede@outlook.com>
2024-06-01 16:17:56 -0300

    Adicionando migrations para as novas entidades

---
+++ codigo-fonte/time-sheet-api/TimeSheet/Infrastructure/Migrations/20240601191600_AddPendingJourneyModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeSheet.Infrastructure.Migrations
+{
+    public partial class AddPendingJourneyModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "JourneyType",
+                table: "WorkJourneys",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "PendingJourneyEntries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Date = table.Column<DateTime>(type: "date", nullable: false),
+                    StartTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    EndTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    StartLunchTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    EndLunchTime = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PendingJourneyEntries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PendingJourneyEntries");
+
+            migrationBuilder.DropColumn(
+                name: "JourneyType",
+                table: "WorkJourneys");
+        }
+    }
+}
