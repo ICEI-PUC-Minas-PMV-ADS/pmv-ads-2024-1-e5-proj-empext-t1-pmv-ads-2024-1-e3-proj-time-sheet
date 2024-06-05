c5d4e5e292606f39d01ed332cd3a38a00a5e1ce2
RaulShinaede <rshinaede@outlook.com>
2024-06-05 20:09:44 -0300

    Adicionando migrations

---
+++ codigo-fonte/time-sheet-api/TimeSheet/Infrastructure/Migrations/20240605230836_RemovingPendingEntryModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeSheet.Infrastructure.Migrations
+{
+    public partial class RemovingPendingEntryModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalTime",
+                table: "Users",
+                newName: "WorkTime");
+
+            migrationBuilder.AddColumn<int>(
+                name: "JourneyType",
+                table: "WorkJourneys",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JourneyType",
+                table: "WorkJourneys");
+
+            migrationBuilder.RenameColumn(
+                name: "WorkTime",
+                table: "Users",
+                newName: "TotalTime");
+        }
+    }
+}
